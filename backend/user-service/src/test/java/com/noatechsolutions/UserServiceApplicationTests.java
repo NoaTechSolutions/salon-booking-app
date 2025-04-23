92e0ed82062871218caf36a8f3b0cad86a042607
IsraelEsparza <noatechsolutions@gmail.com>
2025-04-22 21:30:10 -0700

    Initial commit Project setup

---
+++ backend/user-service/src/test/java/com/noatechsolutions/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noatechsolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
